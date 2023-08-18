577776a6846ac39663b79837e32e35aa1d3ac284
Eliška Klimtová <131253274+eliskaKli@users.noreply.github.com>
2023-08-18 12:12:28 +0200

    addContact & addAppointment adds

--- appointment-planner-starting-v18/src/containers/appointmentsPage/AppointmentsPage.js
+++ appointment-planner-starting-v18/src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,26 +3,48 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
+export const AppointmentsPage = (props) => {
   /*
   Define state variables for 
   appointment info
   */
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleContactChange = (e) => setContact(e.target.value);
+  const handleDateChange = (e) => setDate(e.target.value);
+  const handleTimeChange = (e) => setTime(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    alert("New contact is submitting :)");
     /*
     Add contact info and clear data  
     */
-   
   };
 
   return (
     <div>
-      <section>
+      <section type="submit" onSubmit={handleSubmit}>
         <h2>Add Appointment</h2>
+        <label>
+          Title:
+          <input type="text" value={title} onChange={handleTitleChange} required />
+        </label>
+        <label>
+          Contact:
+          <input type="text" value={contact} onChange={handleContactChange} required />
+        </label>
+        <label>
+          Date:
+          <input type="date" value={date} onChange={handleDateChange} required />
+        </label>
+        <label>
+          Time:
+          <input type="time" value={time} onChange={handleTimeChange} required />
+        </label>
+        <button type="submit">Submit new appointment</button>
       </section>
       <hr />
+
       <section>
         <h2>Appointments</h2>
       </section>
--- appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
+++ appointment-planner-starting-v18/src/containers/contactsPage/ContactsPage.js
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = (props) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const handleChangedName = (e) => setName(e.target.value);
+  const handleChangedPhoneNum = (e) => setPhoneNum(e.target.value);
+  const handleChangedEmail = (e) => setEmail(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,9 +27,40 @@ export const ContactsPage = () => {
 
   return (
     <div>
-      <section>
-        <h2>Add Contact</h2> 
-      </section>
+      <form onSubmit={handleSubmit}>
+        <h2>Add Contact</h2>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={handleChangedName}
+            defaultValue={"FirstName LastName"}
+            required
+          ></input>
+        </label>
+        <label>
+          Phone number:
+          <input
+            type="number"
+            value={phoneNum}
+            onChange={handleChangedPhoneNum}
+            required
+          ></input>
+        </label>
+        <label>
+          email:
+          <input
+            type="text"
+            value={email}
+            onChange={handleChangedEmail}
+            required
+          ></input>
+        </label>
+        <button type="submit">
+          Submit new contact
+        </button>
+      </form>
       <hr />
       <section>
         <h2>Contacts</h2>
